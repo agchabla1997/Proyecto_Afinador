90c6f461ca504424ef1b82378243bd92db9913d7
agchabla1997 <95325190+agchabla1997@users.noreply.github.com>
2022-06-17 00:06:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ from PyQt5 import uic
 from PyQt5.QtCore import QByteArray
 import time
 import publicador
+
+class Proceso(QObject): 
+  def __init__(self):
+        super(Proceso, self).__init__()
+  def procesoPub(self):
+        publicador.PublicaNota()
+
+ class Ventana(QMainWindow):
+  def __init__(self):
+        super(Ventana, self).__init__()
+        uic.loadUi("disenofinal.ui", self)
+
